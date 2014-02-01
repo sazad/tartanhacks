dcd307835ba9e545d8514f7dd95466bafe39d6b3
sazad <sazad@andrew.cmu.edu>
2014-02-01 15:32:05 -0500

    EVERYTHING NEW :)
    changes still to come

--- web.py
+++ web.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, render_template
+import pymysql
 app = Flask(__name__)
+
+db = pymysql.connect(host="localhost", user="root", passwd="root", db="textbooks")
+
+cur = db.cursor() 
 
 @app.route("/")
 def beginning():
@@ -13,22 +18,34 @@ archive = []
 
 @app.route("/textbook/new", methods=["POST"])
 def addTextbook():
-	price = int(request.form['price'])
-	cNum = request.form['courseNum']
-	email = request.form['email']
-	name = request.form['textName']
-	new = Book(price, cNum, email, name)
-	archive.append(new)
-	archive.sort(key = lambda x: x.price)
-	return render_template('saleposted.html')
-    
+    # Print statement here
+    print "inside add textbook"
+    print request.method
+    print "trying forms"
+    cNum = request.form['courseNum']
+    email = request.form['email']
+    name = request.form['textName']
+    print cNum
+    print email
+    print name
+    print "printing forms"
+    price = request.form['price']
+    print price
+    addToDatabase(price, cNum, email, name)
+    return render_template('saleposted.html')
+
+def addToDatabase(price, cNum, email, name):
+    cur.execute("INSERT INTO textbooksNEW (email, courseNum, bookName, price) VALUES ('"+ email +"', '"+ cNum +"', '"+ name +"', "+ price +")")
+
 @app.route("/textbook/buyer")
 def allTextbooks():
 	return render_template('buyer.html')
 
 @app.route("/textbook/search", methods=["POST"])
 def searchTextbooks():
-	return render_template('textbooks.html', textbooks=archive, course=request.form['course'])
+	courseNumber = request.form['course']
+	cur.execute("SELECT '"+ courseNumber +"' FROM textbooksNEW WHERE courseNum= '"+ courseNumber+"' ORDER BY price ASC")
+	return render_template('textbooks.html', textbooks=cur.fetchall())
 
 class Book:
 	def __init__(self, price, course_no,email, name):

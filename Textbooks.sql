1b9c0e4b406ed632b297f3f7c2c377cb710bcd67
Sonia <sazad@andrew.cmu.edu>
2014-07-11 15:10:04 -0500

    ?

--- Textbooks.sql
+++ Textbooks.sql
@@ -2,5 +2,12 @@ CREATE TABLE textbooks (
 	email varchar(40),
 	courseNum varchar(10),
 	bookName varchar(100),
-	price DECIMAL(3, 2)
+	price DECIMAL(10, 5)
 );
+
+drop table textbooks
+ 
+select * from textbooks;
+
+ALTER TABLE textbooks 
+ALTER column 
